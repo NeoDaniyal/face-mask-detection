da679cfd1edce63f8d99a741d5bbf84ac6f94fbb
Daniyal <syeddaniyal1016@gmail.com>
2026-09-05 17:07:58 +0530

    Create 4-stage convolutional backbone, gobal flattering and droupout regularization

---
+++ src/model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+
+class MaskCNNBaseline(nn.Module):
+    """Custom 4-stage CNN baseline for binary face mask classification."""
+    def __init__(self, dropout_rate: float =0.5)->None:
+        super().__init__()
+
+        # Stage 1: 3->32 channels | 224x224 -> 112x112
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        # Stage 1: 3->32 channels | 112x112 -> 56x56
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        # Stage 1: 3->32 channels | 112x112 -> 56x56
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(32 * 112 * 112, 128),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_rate),
+            nn.Linear(128, 1),
+        )
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        logits = self.classifier(x)
+        return logits
