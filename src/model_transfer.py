af69dad04d67c01a94c95d75f791215cb61cb853
Daniyal <syeddaniyal1016@gmail.com>
2026-09-07 16:57:45 +0530

    Create src/model_transfer.py to define the transfer learning model

--- src/model_transfer.py
+++ src/model_transfer.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18, ResNet18_Weight
+from torchvision.models import resnet18, ResNet18_Weights
 
 class MaskResNet18(nn.Module):
     "ResNet-18 model fine-tuned for binary face mask classification."
     def __init__(self, freeze_backbone: bool =False, dropout_rate: float= 0.3)->None:
         super().__init__()
-        weights = ResNet18_Weight.DEFAULT
+        weights = ResNet18_Weights.DEFAULT
         backbone = resnet18(weights=weights)
     #Optional: freeze initial convolutional layers for feature extraction
         if freeze_backbone:
